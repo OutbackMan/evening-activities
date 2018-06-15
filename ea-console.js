5a89dfd6b46b2fbb23a14ccef941a61783a3f503
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-06-15 20:40:26 +1000

    Update ea-console.js

--- ea-console.js
+++ ea-console.js
@@ -1,7 +1,13 @@
+import * as EA from "./modules/ea.js" // export function name() {}
+import * as Log from "./modules/utils/log.js"
+import * as Common from "./modules/common.js"
+
 const RUNNING_IN_BROWSER = false
 
-function ea_console() {
-	!RUNNING_IN_BROWSER && log("msg")
+function ea_console(event) {
+	!RUNNING_IN_BROWSER && Log.log("msg")
+		
+	EA.run() // typeof, nodeName
 }
 
 document.addEventListener("DOMContentLoaded", ea_console);
