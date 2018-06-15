3c35c504592e0e83c5322493d4d90a5ac62f2dff
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-06-15 21:33:21 +1000

    Update ea-console.js

--- ea-console.js
+++ ea-console.js
@@ -2,51 +2,20 @@ import * as EA from "./modules/ea.js"; // export function name() {}
 import * as Log from "./modules/utils/log.js";
 import * as Common from "./modules/common.js"; // include export class ArgumentError()
 
-/* MACROS
-const RUNNING_IN_BROWSER = false;
-!RUNNING_IN_BROWSER && Log.info("msg");
+/* ENUMS
+const LEVELS = Object.freeze({"DEBUG": "0", "RELEASE": "1"});
 */
+const IN_DEBUG_MODE = true;
+const WANT_PHANTOM_JS_EXECUTION = false;
 
-function ea_console(event) {
-	Log.set_level(Log.LEVELS.DEBUG);
-		
+function main(event) {
+	IN_DEBUG_MODE && console.log("test");
+	IN_DEBUG_MODE && console.warn("test2");
+	console.error("test3");
+	
 	EA.run(); // typeof, nodeName
 	
-	phantom.exit();
+	WANT_PHANTOM_JS_EXECUTION && phantom.exit();
 }
 
-document.addEventListener("DOMContentLoaded", ea_console);
-
-// log.js
-export const LEVELS = Object.freeze({"DEBUG": "0", "RELEASE": "1"});
-
-let current_log_level = LEVELS.DEBUG;
-const INFO_CONSOLE_HANDLE = console.log.bind(window.console); // use these to preserve line numbers
-const WARN_CONSOLE_HANDLE = console.warn.bind(window.console);
-const ERROR_CONSOLE_HANDLE = console.error.bind(window.console);
-
-
-export function set_level() {
-	// check argument type
-	current_log_level = desired_log_level;
-}
-
-export function info(msg) {
-	if (current_log_level < LEVELS.RELEASE) {
-		console.log(msg);
-	} else {
-		return;
-	}	
-}
-
-export function warn(msg) {
-	if (current_log_level < LEVELS.RELEASE) {
-		console.warn(msg);
-	} else {
-		return;
-	}
-}
-
-export function error(msg) {
-	console.error(msg);
-}
+document.addEventListener("DOMContentLoaded", main);
