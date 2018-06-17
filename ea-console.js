82fdc89d0cc456dde9964100346032130850ae07
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-06-18 08:41:50 +1000

    Update ea-console.js

--- ea-console.js
+++ ea-console.js
@@ -1,21 +1,23 @@
 import * as EA from "./modules/ea.js"; // export function name() {}
-import * as Log from "./modules/utils/log.js";
 import * as Common from "./modules/common.js"; // include export class ArgumentError()
+import * as CONFIG from "./modules/config.js";
 
 /* ENUMS
 const LEVELS = Object.freeze({"DEBUG": "0", "RELEASE": "1"});
-*/
-const IN_DEBUG_MODE = true;
-const WANT_PHANTOM_JS_EXECUTION = false;
 
-function main(event) {
-	IN_DEBUG_MODE && console.log("test");
-	IN_DEBUG_MODE && console.warn("test2");
+constants, enums, module namespace == UPPER_SNAKE_CASE
+classes, objects == ThisCase
+functions, variables == lower_snake_case
+*/
+
+function main(listened_event) {
+	CONFIG.haIN_DEBUG_MODE && console.log("test");
+	CONFIG.IN_DEBUG_MODE && console.warn("test2");
 	console.error("test3");
 	
 	EA.run(); // typeof, nodeName
 	
-	WANT_PHANTOM_JS_EXECUTION && phantom.exit();
+	CONFIG.WANT_PHANTOM_JS_EXECUTION && phantom.exit();
 }
 
 document.addEventListener("DOMContentLoaded", main);
